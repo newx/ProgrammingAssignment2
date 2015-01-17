9575946af1c08db6764da2ea51ade9c92c00610d
newxcombr <newxhost@gmail.com>
2015-01-17 21:40:15 -0200

    add comments to methods

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,11 @@
+#Creates a special "matrix" object that can cache its inverse
+# Args:
+#   x: a square invertible matrix object
+#
+# Returns:
+#   A special "matrix" based on a list object that provides 
+#   a get/set methods to the original matrix object and a
+#   get/set methods to the inversed (and cached) matrix
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
   set <- function(y) {
@@ -12,7 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-
+# Checks whether or not whe have a cached inversed matrix. 
+# Returns the cached object if found, otherwise computes the inverse matrix using R "solve" method
+#
+# Args:
+#   x: a square invertible matrix object
+#
+# Returns:
+#   An inverse matrix of x matrix (cached or computed)
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinverse()
